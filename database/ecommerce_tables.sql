474182944ff42ae865941f128304159d8edfaeba
ra2257006 <gabriel.c.salzedas@unesp.br>
2023-09-12 08:21:56 -0300

    Hover do vídeo arrumado - Fernando

---
+++ database/ecommerce_tables.sql
@@ -0,0 +1,34 @@
+create table categorias(
+    id_categorias serial primary key,
+    nome text not null,
+    descricao text
+);
+
+create table produtos(
+    id_produto serial primary key,
+    nome text not null,
+    preco decimal(10,2) not null,
+    descricao text,
+    imagem varchar(255) not null,
+    excluido boolean default false,
+    data_exclusao timestamp,
+    codigovisual varchar(50) not null,
+    custo decimal(10,2) not null,
+    margem_lucro decimal(10,2) not null,
+    icms decimal(10,2) not null,
+    categoria_id integer not null,
+    quantidade_estoque integer not null,
+    foreign key (categoria_id) references categorias(id_categorias)
+);
+
+create table usuarios(
+    id_usuario serial primary key,
+    nome text not null,
+    email text not null,
+    senha varchar(255) not null,
+    data_cadastro timestamp not null,
+    telefone varchar(14) not null,
+    cpf varchar(11) not null,
+    endereco varchar(255),
+    cep varchar(8)
+);
