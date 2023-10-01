0fa76d4bb438e057c4d5e52c764359ca3ea1e1df
Sekqies <fernando.ellerbrock@gmail.com>
2023-10-01 18:41:09 -0300

    Feito efeito de filtro

--- website/script_produtos.js
+++ website/script_produtos.js
@@ -2,16 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
 const filtro = document.getElementById('filtro');
 const seta = document.querySelector('#filtro img');
 const dropdown = document.getElementById('dropdown');
-
+let chave = true;
 filtro.addEventListener('click', () => {
-  if (dropdown.style.display == 'none') {
-    dropdown.style.display = 'block';
+  if (chave) {
+    filtro.style.borderColor = 'var(--cor-interac)';
+    dropdown.style.animation= 'mostra-dropdown 0.4s forwards';
     seta.style.transform = 'rotate(180deg)';
   }
   else {
-    dropdown.style.display = 'none';
+    filtro.style.borderColor = 'grey';
+    dropdown.style.animation = 'esconde-dropdown 0.4s forwards';
     seta.style.transform = 'rotate(0deg)';
   }
+  chave = !chave;
 });
 
 });
