c901f45a71ed1e5c324e71f1c620fb4372f2a74b
Sekqies <fernando.ellerbrock@gmail.com>
2023-10-02 12:11:00 -0300

    Terminado visual do filtro, iniciado javascript

--- website/script_produtos.js
+++ website/script_produtos.js
@@ -17,4 +17,25 @@ filtro.addEventListener('click', () => {
   chave = !chave;
 });
 
+
+const checkboxes = document.querySelectorAll('.checkboxes input[type=checkbox]');
+const corpo = document.getElementById('corpo-filtro-display');
+
+function criarElemento(nome){
+  console.log("Palmeiras");
+  let htmlString = '<div class="filtro-display">';
+  htmlString += nome + '</div>';
+  corpo.innerHTML = htmlString;
+}
+for (let i=0; i<checkboxes.length; i++) {
+  console.log("Palmeiras");
+  checkboxes[i].addEventListener('change', () => {	
+    console.log("Mudou o " + this.getAttribute('name'));
+    if (this.checked)
+    {
+      let nome = this.getAttribute('name');
+      criarElemento(nome);
+    }
+  });
+}
 });
