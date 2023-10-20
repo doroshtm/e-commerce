dc9193019d12c19fd8110b687259ea48ae7a9487
Sekqies <fernando.ellerbrock@gmail.com>
2023-10-20 09:27:58 -0300

    Feita landing page

--- website/index.php
+++ website/index.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="./styles_header_footer.css">
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;1,9..40,400;1,9..40,500&display=swap" rel="stylesheet">         
         <link rel="stylesheet" href="styles_paginas.css">
+        <script src="./js/home.js"> </script>
         <link rel="icon" type="image/svg+xml" href="./imagens/MC_Logo_Footer.svg">
     </head>
     <body>    
@@ -22,14 +23,121 @@
         </header>
     <div id="content">
         <div class="container-geral">
-            <div id="display-produtos">
-            </div>
-            <div id="container-bolas">
-                <div class="bolamaior">
-                    <div class="bolamenor"></div>
+        <div id="display-produtos">
+        <div id="oquemostra">
+            <div class="linha direita">
+                <div class="container-produtos-flutuantes">
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/rendercti.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Renderpython.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Rendertouro.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Renderlobo.png">
+                    </div>
                 </div>
-                <div class="bolamaior"></div>
+                <div class="container-produtos-flutuantes">
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/rendercti.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Renderpython.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Rendertouro.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Renderlobo.png">
+                    </div>
+                </div>
             </div>
+            <div class="linha esquerda">
+                <div class="container-produtos-flutuantes">
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/renderglitch.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Renderjussara.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Rendereureka.png">
+                    </div>
+                    <div class="produto-flutuante">
+                        <img src="./imagens/gatoCTI.png">
+                    </div>
+                </div>
+                <div class="container-produtos-flutuantes">
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/renderglitch.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Renderjussara.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Rendereureka.png">
+                    </div>
+                    <div class="produto-flutuante">
+                        <img src="./imagens/gatoCTI.png">
+                    </div>
+                </div>
+            </div>
+            <div class="linha direita">
+                <div class="container-produtos-flutuantes">
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/info.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/mec.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/elet.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/Emblema_Mascotero.svg">
+                    </div>
+                </div>
+                <div class="container-produtos-flutuantes">
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/info.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/mec.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/elet.png">
+                    </div>
+                    <div class ="produto-flutuante">
+                        <img src="./imagens/cti.png">
+                    </div>
+                </div>
+            </div>
+            <div id="cobertura-vidro">
+                 <div id="conteudo-vidro">
+                    <div id="texto-vidro">
+                        <span> Venha pegar o seu </span>
+                        <span id="destaque">mascote da sorte</span>
+                        <span id="descricao">
+                            Chaveiros de pano e adesivos 
+                            com ilustrações originais de mascotes do CTI 
+                            para te dar sorte no vestibulinho!
+                        </span>
+                        <a href="produtos.php">
+                            <div id="botao-produto-home">
+                                Obtenha já o seu!
+                            </div>
+                        </a>
+                    </div>
+                    <div id="imagem-vidro">
+                        <img src="./imagens/cti_sticker.png">
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
             <span class="texto-destaque">O que fazemos</span>
             <div id="container-mascote-produtos">
                 <div class="conteudo-mp">
