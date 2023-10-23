adb010ff52ff299e9d53162250aed81e110c6f05
Gabriel Carraro Salzedas <gabrielcreeper@outlook.com>
2023-10-22 22:55:35 -0300

    Consertada irracionalidade no carrinho.php parte 3

--- website/js/carrinho.js
+++ website/js/carrinho.js
@@ -2,7 +2,7 @@
 // recarregar a página para adicionar um produto ao carrinho, somente para descobrir que não há estoque suficiente para a quantidade que ele deseja comprar.
 // O site também fica mais bonito, porque o PHP não carrega a página inteira se houver um erro, somente o alerta.
 function verificateAdd(amount, stock, id) {
-    if (amount+1 > stock) {
+    if (amount > stock) {
         alert("Não há estoque suficiente para essa quantidade!");
     } else {
         window.location.href = "carrinho.php?id=" + id +"&action=add&amount=1&url=carrinho.php";
