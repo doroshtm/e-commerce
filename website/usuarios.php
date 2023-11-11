2730331fdfcd5c8702ed68beb2f61c8f4a6fa0ab
Gabriel Carraro Salzedas <gabrielcreeper@outlook.com>
2023-11-11 14:20:19 -0300

    Alterado relatório para seguir pedidos do Cabello

--- website/usuarios.php
+++ website/usuarios.php
@@ -11,11 +11,8 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles_header_footer.css">
-    <link rel="stylesheet" href="styles_paginas.css">
     <link rel="icon" type="image/svg+xml" href="./imagens/MC_Logo_Footer.svg">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles_header_footer.css">
-    <link rel="stylesheet" href="styles_paginas.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap">
     <link rel="icon" type="image/svg+xml" href="./imagens/MC_Logo_Footer.svg">
     <title>Usuário | Mascotero</title>
